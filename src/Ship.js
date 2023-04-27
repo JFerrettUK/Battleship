0a711a000964072d6d20d474f221457dd1df0127
JFerrettUK <jferrettuk@gmail.com>
2023-04-27 09:24:46 +0100

    Webpack installed, Jest installed

---
+++ src/Ship.js
@@ -0,0 +1,6 @@
+function testFunction(a, b) {
+  console.log("test is running");
+  return a + b;
+}
+
+module.exports = testFunction;
---
+++ src/Ship.test.js
@@ -0,0 +1,5 @@
+const Ship = require("./Ship");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(Ship(1, 2)).toBe(3);
+});
