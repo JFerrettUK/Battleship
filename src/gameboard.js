f2a2394e4efff51fee6815e6ea79492b052ca06c
JFerrettUK <jferrettuk@gmail.com>
2023-04-30 12:23:20 +0100

    placeShip function initialised on Gameboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,16 +1,32 @@
-export default function gameboard(length) {
-  let newGameboard = function (length) {
-    let board = [];
+import ship from "./ship";
 
+export default function gameboard(length) {
+  let thisBoard = [];
+
+  for (let i = 0; i < 10; i++) {
+    let row = [];
     for (let i = 0; i < 10; i++) {
-      let row = [];
-      for (let i = 0; i < 10; i++) {
-        row.push(null);
-      }
-      board.push(row);
+      row.push(null);
     }
-    return board;
+    thisBoard.push(row);
+  }
+
+  let placeShip = function (length, row, column) {
+    for (let i = column; i < length + column; i++) {
+      thisBoard[row][i] = `ship${length}`;
+    }
+    //ship.length is how long it will be
+    // code to place ship at x,y
+    console.log(thisBoard);
   };
 
-  return newGameboard();
+  let receiveAttack = function (x, y) {
+    // code to receive attack at x,y
+  };
+
+  let allSunk = function () {
+    // code to check if all ships are sunk
+  };
+
+  return { board: thisBoard, placeShip, receiveAttack, allSunk };
 }
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,28 +1,28 @@
 import gameboard from "./gameboard";
+import ship from "./ship";
 
 test("testGameboard has the correct number of rows (10)", () => {
-  let testGameboard = gameboard();
-  expect(testGameboard.length).toBe(10);
+  let battleshipBoard = gameboard();
+  expect(battleshipBoard.board.length).toBe(10);
 });
 
 test("testGameboard row arrays are all the correct length (10)", () => {
-  let testGameboard = gameboard();
+  let battleshipBoard = gameboard();
   for (let i = 0; i < 10; i++) {
-    expect(testGameboard[i].length).toBe(10);
+    expect(battleshipBoard.board[i].length).toBe(10);
   }
 });
 
 // Gameboards should be able to place ships at specific
 // coordinates by calling the ship factory function.
 
-// Gameboards should have a receiveAttack function
-// that takes a pair of coordinates, determines whether
-// or not the attack hit a ship and then sends the ‘hit’
-// function to the correct ship, or records the coordinates
-// of the missed shot.
-
-// Gameboards should keep track of missed attacks so they
-// can display them properly.
-
-// Gameboards should be able to report whether or not all
-// of their ships have been sunk.
+test("place ship with vertical length at specific coordinates", () => {
+  let battleshipBoard = gameboard();
+  battleshipBoard.placeShip(3, 2, 1);
+  expect(battleshipBoard.board[2][1]).toBe("ship3");
+  expect(battleshipBoard.board[2][2]).toBe("ship3");
+  expect(battleshipBoard.board[2][3]).toBe("ship3");
+  //I don't need to actually place the ship on each square! Just store an object in the gameboard
+  //Then add a hit counter whenever something "hits" that square.
+  //Keep the gameboard list and ship objects totally seperate!
+});
